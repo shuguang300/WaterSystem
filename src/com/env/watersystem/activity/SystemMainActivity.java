6bafaa8acd9a8b35ad046b971e6ccb4de903a584
shuguang300 <shuguang300@gmail.com>
2014-08-15 16:59:59 +0800

    actionbar bug

--- src/com/env/watersystem/activity/SystemMainActivity.java
+++ src/com/env/watersystem/activity/SystemMainActivity.java
@@ -1,6 +1,7 @@
 package com.env.watersystem.activity;
 import com.env.watersystem.R;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,11 +12,23 @@ import android.widget.Button;
 public class SystemMainActivity extends Activity {
 	
 	private Button btn_Device,btn_Patrol;
+	private ActionBar actionBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.system_activity_main);
+		iniView();
+	}
+	
+	private void iniActionBar(){
+		actionBar = getActionBar();
+		actionBar.setDisplayShowHomeEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+	
+	private void iniView(){
+		iniActionBar();
 		btn_Device = (Button)findViewById(R.id.system_activity_main_btn_device);
 		btn_Patrol = (Button)findViewById(R.id.system_activity_main_btn_patrol);
 		btn_Device.setOnClickListener(new OnClickListener() {
